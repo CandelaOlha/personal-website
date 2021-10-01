a1558b4b8f29e48c8cfc1a2b3a9868b8845b1c67
CandelaOlha <olhacandela@gmail.com>
2021-10-01 18:30:31 -0300

    added a new function for mobile menu links

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-console.log("hola")
-
 const openMobileMenu = document.querySelector(".hamburger-menu");
 const closeMobileMenu = document.querySelector(".close-mobile-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuLinks = document.querySelectorAll(".mobile-menu-link");
+
+// Mobile menu functions
 
 openMobileMenu.onclick = () => {
     mobileMenu.classList.add("show-mobile-menu");
@@ -10,4 +11,10 @@ openMobileMenu.onclick = () => {
 
 closeMobileMenu.onclick = () => {
     mobileMenu.classList.remove("show-mobile-menu");
+}
+
+for (let i = 0; i < mobileMenuLinks.length; i++) {
+    mobileMenuLinks[i].onclick = () => {
+        mobileMenu.classList.remove("show-mobile-menu");
+    }
 }
